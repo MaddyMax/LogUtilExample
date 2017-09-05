258dfa6380b6c482c38573b119459165794a1482
MaddyMax <kparesh409@gmail.com>
2017-09-05 11:27:47 +0530

    Initial commit

---
+++ loglib/src/main/java/LogPrint.java
@@ -0,0 +1,13 @@
+import android.util.Log;
+
+/**
+ * Created by maddy on 5/9/17.
+ */
+
+public class LogPrint {
+    private String logTag = "Log Gradle Tag";
+
+    LogPrint(String value){
+        Log.d(logTag,value);
+    }
+}
